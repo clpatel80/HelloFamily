9f1977d46b61a66e1d542b2a1d6ab43eb5106737
clpatel80 <clpatel80@gmail.com>
2018-07-05 19:07:53 -0400

    adding images

---
+++ adventure.js
@@ -0,0 +1,28 @@
+/*<script>
+
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
+
+</script>*/
+
+function check_email(email){  
+    var reg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,7})+$/;
+    if(reg.test(email)){
+        return true;
+    }else{  
+        return false;
+    }  
+}  
