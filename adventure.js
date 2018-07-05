262c0a5e6c44e7d1a9fba53946491b9c68ddd0bf
clpatel80 <clpatel80@gmail.com>
2018-07-05 19:33:32 -0400

    updated project files

--- adventure.js
+++ adventure.js
@@ -2,7 +2,7 @@
 
 function ValidateEmail(inputText)
 {
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var mailformat = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,7})+$/;
 if(inputText.value.match(mailformat))
 {
 document.form1.text1.focus();
@@ -26,3 +26,15 @@ function check_email(email){
         return false;
     }  
 }  
+
+function validateEmail(email) {
+    // First check if any value was actually set
+    if (email.length == 0) return false;
+    // Now validate the email format using Regex
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    return re.test(email);
+}
+
+var email = document.getElementById('email');
+if (validateEmail(email)) { alert('Valid email address'); }
+else { aler('Invalid email address'); }
